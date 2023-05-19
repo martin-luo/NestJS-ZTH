12ce9030dda06d063baef43e7bcde4ba8c1dee85
Martin Luo <aluo1.developer@gmail.com>
2023-05-19 16:23:27 +1000

    feat: use nestjs ConfigModule (#5)

--- nestjs-task-management/src/tasks/tasks.controller.ts
+++ nestjs-task-management/src/tasks/tasks.controller.ts
@@ -18,13 +18,17 @@ import { Task } from './task.entity';
 import { AuthGuard } from '@nestjs/passport';
 import { User } from '../auth/user.entity';
 import { GetUser } from '../auth/get-user.decorator';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('tasks')
 @UseGuards(AuthGuard())
 export class TasksController {
   private logger = new Logger('TasksController', true);
 
-  constructor(private tasksService: TasksService) {}
+  constructor(
+    private tasksService: TasksService,
+    private configService: ConfigService,
+  ) {}
 
   @Get()
   getAllTasks(
